4bd3822ddba35958882f54cc349f891ba222b146
caseymccullough <kcmccullough@gmail.com>
2021-04-28 20:04:19 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -31,8 +31,6 @@ app.get('/greeting/:name', (req, res) => {
    res.send(`<h1>Hello ${req.params.name}</h1>`)
 })
 
-
-
 app.get('/tip/:total/:tipPercentage', (req, res) => {
    const total = parseFloat(req.params.total);
    const tipPercentage = parseFloat(req.params.tipPercentage);
@@ -41,10 +39,28 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
        res.send(`<h1>The percentage must be between 0.0 and 100.0</h1>`)
    } else {
        const tipAmount = tipPercentage * total / 100.;
-
        res.send(`<h1>Your tip: $${Number(tipAmount).toFixed(2)}`)
    }
 })
+
+app.get('/magic', (req, res) => {
+   res.send (`<h1>Magic Eight Ball</h1>`)
+})
+
+app.get('/magic/:question', (req, res) => {
+
+   const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", 
+   "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+   randPos = Math.floor(Math.random() * responses.length)
+   const question = req.params.question;
+   const answer =  responses[randPos];
+
+   res.send (` <h1>${question}?<h1>
+   <h2>Magic Eight Ball says: ${answer}</h2>`)
+})
+
+
 // New <----- dont need this
 // Delete 
 // Update 
